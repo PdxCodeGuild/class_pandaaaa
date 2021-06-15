98d68e6c34f938dd0c3b2b009845d941d434701b
Brea <breabrown96@gmail.com>
2021-06-15 11:46:38 -0700

    lab 1

---
+++ Code/BreaBrown/lab_01.py
@@ -0,0 +1,17 @@
+conversions = {
+'ft': 0.3048,
+'mi': 1009.34,
+'m': 1,
+'km': 1000,
+'yards': 0.9144,
+'inches': 0.0254
+}
+
+
+ft_in_m = 0.3048
+distance = int(input('What\'s the distance? '))
+start_unit = input('Enter the type of unit: ')
+end_unit = input('Enter the unit you\'d like to convert to: ')
+output = distance * conversions[start_unit]  / conversions[end_unit]
+distance = float(distance)
+print(f'{distance} {start_unit} in {end_unit} is {output} {end_unit}.')
