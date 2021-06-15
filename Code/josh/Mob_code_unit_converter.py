1305e9b841bc19a930186fa5b051df8c248a457f
SB <sarahbtracy17@gmail.com>
2021-06-15 16:35:43 -0400

    version three

--- Code/josh/Mob_code_unit_converter.py
+++ Code/josh/Mob_code_unit_converter.py
@@ -1,5 +1,11 @@
-convert_dict = { "ft" : 0.3048, "mi": 1609.34, "m": 1, "km": 1000 }
-
+convert_dict = { 
+  "ft" : 0.3048, 
+  "mi": 1609.34, 
+  "m": 1, 
+  "km": 1000,
+  "yd": 0.9144,
+  "in": 0.0254
+   }
 
 def main():
   # print(str(unit_converter(distance_input())) + "m")
@@ -10,7 +16,6 @@ def main():
   print(meters)
   exit(0)
 
-
 def distance_input():
   distance = input("What is the distance?")
   return float(distance)
@@ -19,19 +24,18 @@ def unit_converter(distance_1, unit):
   Answer = distance_1 * convert_dict[unit]
   return Answer
 
-
-  
-
 def unit_input():
   valid = 0 
   while not valid:
-    unit = input("what are the units? (ft / mi / m / km)")
-    if not (unit != "ft" and unit != "mi" and unit != "m" and unit != "km"):
+    unit = input("what are the units? (ft / mi / m / km / yd / in)")
+    if not (unit != "ft" and unit != "mi" and unit != "m" and unit != "km" and unit != "yd" and unit != "in"):
       valid = 1 
   return unit
 
 
-
-
 main()
+
+
+#version three: add support for yards and inches 
+
 
