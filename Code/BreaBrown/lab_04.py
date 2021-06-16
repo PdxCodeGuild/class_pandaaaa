5a848404ef99eb4d1d91be9f81b37a3115b3c91a
Brea <breabrown96@gmail.com>
2021-06-16 13:35:45 -0700

    lab 4 done

--- Code/BreaBrown/lab_04.py
+++ Code/BreaBrown/lab_04.py
@@ -1,37 +1,42 @@
-card_1 = input('First card: ')
-card_2 = input('Second card: ')
-card_3 = input('Third card: ')
+def count_hand():
+    card_value = {'A' : 11,
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '8' : 8,
+    '9' : 9,
+    '10': 10,
+    'J' : 10,
+    'Q' : 10,
+    'K' : 10
+    }
+    points = 0
+    card_1 = input('First card: ')
+    card_2 = input('Second card: ')
+    card_3 = input('Third card: ')
+    card_hand = [card_1, card_2, card_3]
+    for card in card_hand:
+        points += card_value[card]
+        if points >= 10:
+            card_value['A'] = 1
+    return points
+        
+            
+def give_advice(points):
+    if points < 17:
+        output = f'Your total points are {points} you should: "Hit"'
+    elif points >= 17 and points < 21:
+        output = f'Your total points are {points} you should: "Stay"'    
+    elif points == 21:
+        output = f' You have a "Blackjack!" with {points} points'
+    else: 
+        output = f'You have {points} points "Bust!"'    
+    return output
 
-card_value = {'A' : 1,
-'2' : 2,
-'3' : 3,
-'4' : 4,
-'5' : 5,
-'6' : 6,
-'7' : 7,
-'8' : 8,
-'9' : 9,
-'10': 10,
-'J' : 10,
-'Q' : 10,
-'K' : 10
-}
+def run():
+    print(give_advice(count_hand()))
 
-card_1 = card_value[card_1]
-card_2 = card_value[card_2]
-card_3 = card_value[card_3]
-
-hand = card_1 + card_2 + card_3
-
-print(hand)
-
-if hand < 17:
-    output = 'Hit'
-elif hand >= 17 and hand < 21:
-    output = 'Stay'    
-elif hand == 21:
-    output = 'Blackjack!'
-else: 
-    output = 'Bust'    
-
-print(output)
+run()
