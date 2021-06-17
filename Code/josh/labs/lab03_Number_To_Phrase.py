b6f78c745abb8978d3a460139e2407e68002f0c7
jhell85 <jhell85@gmail.com>
2021-06-16 22:13:33 -0700

    caught all edge cases I hope

--- Code/josh/labs/lab03_Number_To_Phrase.py
+++ Code/josh/labs/lab03_Number_To_Phrase.py
@@ -1,6 +1,6 @@
 
 singles_dict = { 
-  "0": "zero",
+  "0": "",
   "1": "one",
   "2": "two",
   "3": "three",
@@ -26,6 +26,7 @@ teen_dict ={
 }
 
 doubles_dict ={
+  "0": "",
   "2": "twenty",
   "3": "thirty",
   "4": "forty",
@@ -45,32 +46,36 @@ def number_converter():
   alpha_number = ""
   if num == "0": #check if user entered "0"
     num_array = []
-    alpha_number = singles_dict["0"]
+    alpha_number = "zero"
   while len(num_array) > 0:
     if num.isnumeric(): 
       if num[0] == "0": # check if user started number with a 0
         num = input(error_response)
         num_array = [n for n in num]
-      elif len(num_array) == 1:
+      elif len(num_array) == 1: 
         number = num_array.pop(0)
-        if number != "0" :#check if the last number is not 0 
+        if number != "0" :# check if the last number is not 0 so not to add zero to the end of a number
           alpha_number += singles_dict[number]
       elif len(num_array) == 2:
-        if num_array[0] == "1": # if the first number of a 2 digit number is a 1
+        if num_array[0] == "1": # if the first number of a 2 number is a 1 need to use teen dict for last 2 digits
           number = num_array.pop(0)
           number_2 = num_array.pop(0)
           alpha_number += teen_dict[number_2]
-        elif num_array[1] == "0": # if the second number of a 2 digit number is 0
+        elif num_array[0] == "0": # if the second number is a 0 remove last digit and evaluate with singles_dict
+          number = num_array.pop(0)
+          number_2 = num_array.pop(0)
+          alpha_number += singles_dict[number_2]
+        elif num_array[1] == "0": # if the second number of a 2 digit number is 0 
           number = num_array.pop(0)
           alpha_number += doubles_dict[number] + " "
-        else:
+        else: 
           number = num_array.pop(0)
           number_2 = num_array.pop(0)
           alpha_number += doubles_dict[number] + " " + singles_dict[number_2]
       elif len(num_array) == 3:
         number = num_array.pop(0)
         if number != "0": 
-          alpha_number += singles_dict[number] + " hundred and "
+          alpha_number += singles_dict[number] + " hundred & "
       elif len(num_array) == 4:
         number = num_array.pop(0)
         if number != "0":
@@ -78,13 +83,13 @@ def number_converter():
       elif len(num_array) == 5:
         number = num_array.pop(0)
         if number == "1":
-          number_2 = num_array.pop(0)
           alpha_number += teen_dict[number_2] + " thousand "
         else:
-          alpha_number += " " + doubles_dict[number] + " " 
+          #Python version of a ternary opperator to remove a space if number is a 0
+          alpha_number += (" " + doubles_dict[number] + " ") if number != "0" else (doubles_dict[number] + " ")
       elif len(num_array) == 6:
         number = num_array.pop(0)
-        alpha_number += singles_dict[number] + " hundred"
+        alpha_number += singles_dict[number] + " hundred &"
       elif len(num_array) == 7:
         number = num_array.pop(0)
         if number != "0": 
@@ -95,10 +100,11 @@ def number_converter():
           number_2 = num_array.pop(0)
           alpha_number += teen_dict[number_2] + " million "
         else:
-          alpha_number += " " + doubles_dict[number] + " " 
+          #Python version of a ternary opperator to remove a space if number is a 0
+          alpha_number += (" " + doubles_dict[number] + " ") if number != "0" else (doubles_dict[number] + " ")
       elif len(num_array) == 9:
         number = num_array.pop(0)
-        alpha_number += singles_dict[number] + " hundred"
+        alpha_number += singles_dict[number] + " hundred &"
       elif len(num_array) > 9:
         num = input(error_response_2)
         num_array = [n for n in num]
