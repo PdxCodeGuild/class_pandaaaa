8c7c1a4745304329be69411b8bf1df5cc61d2087
zmwatts <zachwatts@Zachs-MacBook-Air.local>
2021-06-22 12:56:28 -0400

    Latest updated as requested by Josh

--- PDX_6_14/zach_lab_24.py
+++ PDX_6_14/zach_lab_24.py
@@ -14,10 +14,14 @@ def dataset():
         for line in file:
             date.append(line[0:11])
             rainfall.append(line[16])
-            for j in file:
-                date_dict[line[0:11]] = j[17]
+            try: date_dict[line[0:11]] = int(line[16])
+            except:
+                ValueError
+                date_dict[line[0:11]] = 0
+
     print(date_dict)
     return date, rainfall
+
 
 def max_rainfall(date_, rainfall_):
     most_rain = 0
@@ -25,5 +29,7 @@ def max_rainfall(date_, rainfall_):
         most_rain = day(max(rainfall_))
     print(most_rain)
     return most_rain
+
+
 
 main()
