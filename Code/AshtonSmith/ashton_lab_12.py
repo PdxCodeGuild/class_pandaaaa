6123d7c6c65ea23dfe6df8e7422705c4367b9e48
Ashton Smith <ashton23@pdx.edu>
2021-06-24 10:36:28 -0700

    Lab_12 Completed with tkinter gui

--- Code/AshtonSmith/ashton_lab_12.py
+++ Code/AshtonSmith/ashton_lab_12.py
@@ -1,12 +1,11 @@
-# Let's represent an ATM with a class containing two attributes: 
-#   a balance and an interest rate. 
-#      A newly created account will default to a balance of 0 
-# and an interest rate of 0.1%. 
-# Implement the initializer, as well as the following functions:
+#Ashton Smith
+#Lab12
+#ATM lab
 from tkinter import *
 import tkinter as tk
-from tkinter import messagebox
-
+def main():
+    window = Window()
+    window.mainloop()
 
 #################################################################
 #Window class
@@ -15,100 +14,113 @@ class Window(Tk):
         super(Window, self).__init__()
         self.atm = ATM()
         self.title("ATM")
-        self.minsize(500,500)
+        self.minsize(250,150)
         self.create_radio()
-        self.create_text_box()
+        self.text_box_exists = False
+       
 
    
-   
+    #this function is used to create a text box and button if the button is clicked it calls button clicked
     def create_text_box(self):
-        self.box = Text(width= 5, height= 5)
-        self.box.grid(row = 10 , column = 10)
-        
-        self.textbutton = tk.Button(text='OK', command= self.text_event).grid(row=10, column=12, sticky=tk.W, pady=4)
-        
-    def text_event(self):
+        self.box = Text(width= 5, height= 1)
+        self.text_box_exists = True
+        radSelected = self.radValues.get()
+        if(radSelected == 2):#withdraw
+            self.box.grid(row = 6 , column = 14)
+            self.textbutton = tk.Button(text='OK', command= self.button_clicked)
+            self.textbutton.grid(row=6, column=19, sticky=tk.W, pady=4)
+        if(radSelected == 3):
+            self.box.grid(row = 7 , column = 14)
+            self.textbutton = tk.Button(text='OK', command= self.button_clicked)
+            self.textbutton.grid(row=7, column=19, sticky=tk.W, pady=4)
+            
+    
+    
+    
+    #this function is calls the atm methods. when a rad box is selected: 
+    #       withdraw/deposit: it then creates a text box with a button. 
+    # The button takes the input as a string and then deletes the text box. (and uncheck the rad)
+    def button_clicked(self):
         name = StringVar(self)
         name = self.box.get('1.0', 'end-1c')
         radSelected = self.radValues.get()
-        if(radSelected == 1):#show balance
-            temp = self.atm.balance()
-            self.label4.configure(text = 'Balance :' + str(temp) + '$.')
-        elif(radSelected == 2):#withdraw
-            self.atm.withdraw(float(name))
-            self.label4.configure(text = 'Withdrawed :' + str(name) + '$.')
+        if(radSelected == 2):#withdraw
+            if self.atm.check_withdrawal(float(name)):
+                self.atm.withdraw(float(name))
+                self.label4.configure(text = str(name) + '$ withdrawn.')
+            else:
+                self.label4.configure(text = 'Insufficient Funds')
+            self.box.destroy()
+            self.textbutton.destroy()
+            self.text_box_exists = False
         elif(radSelected == 3):#deposit
             self.atm.deposit(float(name))
             self.label4.configure(text = 'Deposited:' + str(name) + '$.')
-        elif(radSelected == 4):#interest
-            self.label4.configure(text = 'Interest:' + str(name))
-# ws = Tk()
-# ws.title('get text demo')
-# ws.geometry('200x200')
+            self.box.destroy()
+            self.textbutton.destroy()
+            self.text_box_exists = False
 
-# def welcomeMessage():
-#     name = name_Tf.get()
-#     return messagebox.showinfo('message',f'Hi! {name}, Welcome to python guides.')
-    
 
-# Label(ws, text="Enter Name").pack()
-# name_Tf.pack()
-
-# Button(ws, text="Click Here", command=welcomeMessage).pack()
-
-        # self.label4.configure(text = name)
-        #my_str = self.box
-
+    #This function creates radio buttons with the labels: Balance, Withdraw, Deposit, and Interest
     def create_radio(self):
         self.radValues = IntVar()
-        self.rad1 = tk.Radiobutton(self, value = 1, variable = self.radValues , command = self.rad_event)
-        self.rad1.grid(column = 6, row = 5, sticky = W, columnspan = 3)
+        #button
+        self.bal_rad = tk.Radiobutton(self, value = 1, variable = self.radValues , command = self.rad_bal_int)
+        self.bal_rad.grid(column = 6, row = 5, sticky = W, columnspan = 3)
+        #text
         self.label = tk.Label(self, text = 'Balance')
         self.label.grid(column = 5, row = 5)
-
-        self.rad2 = tk.Radiobutton(self, value = 2, variable = self.radValues , command = self.rad_event)
+        #button
+        self.rad2 = tk.Radiobutton(self, value = 2, variable = self.radValues , command = self.rad_dep_with)
         self.rad2.grid(column = 6, row = 6, sticky = W, columnspan = 3)
+        #text
         self.label = tk.Label(self, text = 'Withdraw')
         self.label.grid(column = 5, row = 6)
-       
-        self.rad3 = tk.Radiobutton(self, value = 3, variable = self.radValues , command = self.rad_event)
+        #button
+        self.rad3 = tk.Radiobutton(self, value = 3, variable = self.radValues , command = self.rad_dep_with)
         self.rad3.grid(column = 6, row = 7, sticky = W, columnspan = 3)
+        #text
         self.label = tk.Label(self, text = 'Deposit')
         self.label.grid(column = 5, row = 7)
-
-        self.rad3 = tk.Radiobutton(self, value = 4, variable = self.radValues , command = self.rad_event)
+        #button 
+        self.rad3 = tk.Radiobutton(self, value = 4, variable = self.radValues , command = self.rad_bal_int)
         self.rad3.grid(column = 6, row = 8, sticky = W, columnspan = 3)
+        #text
         self.label = tk.Label(self, text = 'Interest')
         self.label.grid(column = 5, row = 8)
-
-        self.label4 = tk.Label(self, text = 'here')
+        #label used for displaying data from button events(atm functions)
+        self.label4 = tk.Label(self, text = '')
         self.label4.grid(column = 50, row = 20)
+    
+    
 
-    def rad_event(self):
-        temp = ''
+    #radio event for balance and interest 
+    def rad_bal_int(self):
         radSelected = self.radValues.get()
-        if radSelected == 1:
-            temp = str(self.atm.check_balance())
-            self.label4.configure(text = 'Current blance = ' + temp)
-        elif radSelected == 2:
-            #temp = str(self.atm.withdraw())v
-            self.label4.configure(text = temp + '$ withdrawn')
-        elif radSelected == 3:
-            #self.atm.deposit()
-            self.label4.configure(text = temp + '$ deposited')
-        elif radSelected == 4:
-            #temp = self.atm.calc_interest()
-            #self.atm.deposit(temp)
-            temp = str(temp)
-            self.label4.configure(text = temp + '$ interest earned')
-
-        #self.label.grid(column = 50, row = 20)
+        if(radSelected == 1):
+            temp = self.atm.check_balance()
+            self.label4.configure(text = 'Balance :' + str(temp) + '$.')
+        elif(radSelected == 4):
+            self.atm.balance = self.atm.balance + self.atm.calc_interest()
+            self.label4.configure(text = 'Interest Added')
+        
+        #self.text_box_exists = True
+        if(self.text_box_exists == True):
+            self.text_box_exists = False
+            self.box.destroy()
+            self.textbutton.destroy()
 
 
 
-    def take_input():
-        return 0
-         #= inputtxt.get("1.0", "end-1c")
+    #radio event for deposit and withdraw
+    def rad_dep_with(self):
+        temp = ''
+        self.radSelected = self.radValues.get()
+        if(self.text_box_exists == True):
+            self.text_box_exists = False
+            self.box.destroy()
+            self.textbutton.destroy()
+        self.my_text_box = self.create_text_box()
 
 
 
@@ -149,9 +161,11 @@ class ATM():
     # calc_interest() returns the amount of interest calculated on the account
     def calc_interest(self):
         return self.balance * (self.interest_rate)
+
+
+
 #################################################################
-
-
+#This function is used to display a repl for the atm functions
 def use_atm():
     atm = ATM() # create an instance of our class
     print('Welcome to the ATM')
@@ -186,6 +200,8 @@ def use_atm():
             break
         else:
             print('Command not recognized')
-window = Window()
-window.mainloop()
+
+
+
+main()
 #use_atm()
