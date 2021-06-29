84969f638267eb0f0722165e6dad7b0f74cca5c2
Ashton Smith <ashton23@pdx.edu>
2021-06-29 15:07:04 -0700

    lab_15 complete

--- Code/AshtonSmith/ashton_lab_15.py
+++ Code/AshtonSmith/ashton_lab_15.py
@@ -1,20 +1,15 @@
 # Lab 15: Quotes API
 # For this lab we'll be using the Favqs Quotes API to first get a random quote,
-#  and then allow the user to find quotes by keyword. To accomplish this we'll be using the requests library.
-
-# Version 1: Get a Random Quote
-# The URL to get a random quote is https://favqs.com/api/qotd, 
-# send a request here, parse the JSON in the response into a python dictionary, i
-# and show the quote and the author.
 import requests
 import json
+import os
+from dotenv import load_dotenv
+
 
 
 #prog main
 def main():
-    # request_quote()
-
-    request_quote_search()
+    request_quote_search(input("Enter a term to search for: "))
     return 0
 
 
@@ -28,65 +23,35 @@ def request_quote():
     print(data)
 
 
-def request_quote_search():
-    page = '1'
-    term = 'what'
-    # url = f'https://favqs.com/api/quotes?page=<{page}>&filter=<{term}>'
-    # url = 'https://favqs.com/api/quotes?page=1&filter=what'
-    # url = 'https://favqs.com/api/quotes'#/?filter=funny'
-    url = 'https://favqs.com/api/quotes/?filter=funny'
-    headers = {'Authorization': 'Token token="5b8c456532d65b17d851bba193977bf9"'}
-    # print (url)
-    response = requests.get(url, headers)# = {'Accept' : 'application/json'})#, params= {'term':'thou'})#send a get request
-    # data = response.json()
-    # 9
-    # data = json.loads(response.text)
-	# 5b8c456532d65b17d851bba193977bf9
-    # print(data)
-    print(response)
+
+#search by term
+def request_quote_search(term):
+    url = f'https://favqs.com/api/quotes?page=1&filter={term}'
+    last_page = False
+    again = ''
+    payload ={ 
+        'page' : '1',
+        'term' : f'{term}'
+    }
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    headers = {'Authorization': 'Token token="' + str(token) + '"'}
+    count = 1
+    quote_count = 1
+    while not(last_page) and again != 'x':
+        response = requests.get(url, headers= headers, params= payload)
+        data = response.json()
+        for i in data['quotes']:
+            print('Quote ' + str(quote_count) + '.')
+            print (i['body'] + ' - ' + i['author'])
+            print('\n\n')
+            quote_count += 1
+        last_page = data['last_page']
+        print('Page: ' + str(count) + '.')
+        again = input('Again? Enter x to exit or enter to continue to the next page.')
+        payload['page'] = str(int(payload['page']) + 1)
+        count += 1
 
 
-# An app token is required to access our API (except for the Quote of the Day).
 
-# For authorized API calls, pass your access token in via the header:
-
-# Authorization: Token token="YOUR_APP_TOKEN"
-# For example,
-
-# $ curl -H 'Authorization: Token token="YOUR_APP_TOKEN"'
-# Note:  This format will not work:
-
-# Authorization: "YOUR_APP_TOKEN"
 main()
-
-
-
-
-
-
-
-
-
-
-# Version 2: List Quotes by Keyword
-# The Favqs Quote API also supports getting a list of quotes associated with a given keyword 
-# https://favqs.com/api/quotes?page=<page>&filter=<keyword>. 
-# Prompt the user for a keyword, list the quotes you get in response, 
-# and prompt the user to either show the next page or enter a new keyword. You can use string concatenation to build the URL.
-
-# > enter a keyword to search for quotes: nature
-# 25 quotes associated with nature - page 1
-# <list of quotes>
-# > enter 'next page' or 'done': next page
-# 25 quotes associated with nature - page 2
-# <list of quotes>
-# > enter 'next page' or 'done': done
-# > enter a keyword to search for quotes:
-# This API endpoint requires an API key be included in a request header. You can find documentation of specifying request headers here and documentation on authorization with the Favqs API here under "Authorization".
-
-# headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
-# Other Quote APIs
-# Programming Quotes
-# Quotes Garden
-# get random quote https://quote-garden.herokuapp.com/quotes/random
-# get quotes by keyword https://quote-garden.herokuapp.com/quotes/search/<keyword/
