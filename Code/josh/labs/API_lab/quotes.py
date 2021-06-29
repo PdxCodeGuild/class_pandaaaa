5a0c7b3ead39c4c15efc26ac667805c0c1bffbb1
jhell85 <jhell85@gmail.com>
2021-06-29 15:02:07 -0700

    made v2 with search keyword search ability

--- Code/josh/labs/API_lab/quotes.py
+++ Code/josh/labs/API_lab/quotes.py
@@ -1,20 +1,42 @@
+from dotenv.main import find_dotenv
 import requests
+import os
+from dotenv import load_dotenv, find_dotenv
 
-url = 'https://favqs.com/api/qotd'
-headers = {'Content-Type': 'application/json'}
+load_dotenv(find_dotenv())
+KEY = os.environ.get('FAVQ')
+
+url_qotd = 'https://favqs.com/api/qotd'
+
+url_quotes = 'https://favqs.com/api/quotes'
+headers = {'Content-Type': 'application/json',
+            'Authorization': F'Token token={KEY}'
+          }
 
 
 # V1
 def get_quote():
   try:
-    response = requests.get(url, headers=headers)
+    response = requests.get(url_qotd, headers=headers)
     json = response.json()
     print(json)
-  except requests.exceptions.RequestException as e:  # This syntax will catch all errors from a bad response
+  except requests.exceptions.RequestException as e:  
       raise SystemExit(e)
 
 # V2 
+def get_quotes():
+  keyword = input("What would you type of quotes would you like to look for?")
+  print(keyword)
+  try:
+    response = requests.get(url_quotes, headers=headers, params={'filter': keyword })
+    json = response.json()
+    return_str = ""
+    for q in json['quotes']:
+      return_str += F"{q['body']} \n {q['author']} \n \n"
+    print(return_str)
+  except requests.exceptions.RequestException as e:  
+      raise SystemExit(e)
 
 
 
-#get_quote()
+get_quotes()
