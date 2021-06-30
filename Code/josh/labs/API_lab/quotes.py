505937860e64601961e3c944c6993b0ca217a83f
jhell85 <jhell85@gmail.com>
2021-06-30 11:20:40 -0700

    add V2 of quotes lab

--- Code/josh/labs/API_lab/quotes.py
+++ Code/josh/labs/API_lab/quotes.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 KEY = os.environ.get('FAVQ')
-
+# Quote of the day URL
 url_qotd = 'https://favqs.com/api/qotd'
-
+# List of Quotes URL
 url_quotes = 'https://favqs.com/api/quotes'
+
 headers = {'Content-Type': 'application/json',
-            'Authorization': F'Token token={KEY}'
-          }
+            'Authorization': F'Token token={KEY}'}
 
 
 # V1
@@ -19,24 +19,68 @@ def get_quote():
   try:
     response = requests.get(url_qotd, headers=headers)
     json = response.json()
-    print(json)
+    quote = json['quote']
+    print(F"\"{quote['body']}\" \n {quote['author']} \n ")
   except requests.exceptions.RequestException as e:  
       raise SystemExit(e)
 
 # V2 
-def get_quotes():
-  keyword = input("What would you type of quotes would you like to look for?")
-  print(keyword)
+
+# This function searches the API using the keyword passed into it then filters and prints the results
+# also returns False if there are no more quotes for the given keyword
+def get_quotes(keyword, page=1):
   try:
-    response = requests.get(url_quotes, headers=headers, params={'filter': keyword })
+    response = requests.get(url_quotes, headers=headers, params={'filter': keyword, 'page': page })
     json = response.json()
     return_str = ""
     for q in json['quotes']:
-      return_str += F"{q['body']} \n {q['author']} \n \n"
+      author= q['author'] if 'author' in q else ''
+      return_str += F"\"{q['body']}\" \n {author} \n"
     print(return_str)
+    # if return_str == 'No quotes found': # Why doesn't this return True?
+    if json['quotes'][0]['body'] == 'No quotes found':
+      return True
+    else:
+      return json['last_page']
   except requests.exceptions.RequestException as e:  
       raise SystemExit(e)
 
+# This functions takes an input from a user then runs the appropriate function based on the user's input
+# and then takes the return from get_quotes() to evaluate what to ask the user
+def question_looper():
+  counter = 1
+  breaker = True
+  keyword = ''
+  last_page = False
+  while breaker:
+    if last_page:
+      print(F"There are no more quotes for {keyword}.")
+      counter = 1
+      last_page = False
+    elif counter == 1:
+      keyword = input(F"give me a keyword to search for quotes with. \n enter R for a random quote or E to Exit \n").lower()
+      if keyword == "e":
+        breaker = False
+      elif keyword == "r":
+        get_quote()
+      else:
+        last_page = get_quotes(keyword)
+        counter += 1
+    elif counter <= 25:
+      q2 = input(F"Would you like to see more quotes for {keyword}? \n Y = Yes, N = No, E = Exit \n last_page {last_page} counter {counter} \n").lower()  
+      if q2 == "e":
+        breaker = False
+      elif q2 == "y":
+        last_page = get_quotes(keyword, counter)
+        counter += 1
+      elif q2 == "n":
+        counter = 1
+      else:
+        print("sorry that response isn't recognized\n")
+    # this catches if the counter goes past 25 because there are no quotes returned after 25 but the last_page property doesn't return True on page 25
+    else:
+      print(F"There are no more quotes for {keyword}.")
+      counter = 1
+  exit(0)
 
-
-get_quotes()
+question_looper()
