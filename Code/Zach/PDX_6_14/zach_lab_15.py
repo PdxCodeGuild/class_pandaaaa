a149b143b659ecf0a46bcf1c140e76dfc4806700
zmwatts <zachwatts@Zachs-MacBook-Air.local>
2021-07-02 18:34:36 -0400

    updating lab 15 and submitting mini capstone

--- Code/Zach/PDX_6_14/zach_lab_15.py
+++ Code/Zach/PDX_6_14/zach_lab_15.py
@@ -7,13 +7,12 @@ import os
 from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 
-
 def main():
-    user_display()
+    get_quotes()
     exit()
 
 #-----------------------------VERSION 1------------------------------#
-# def get_quotes(term=None,limit=20, page=1):
+# def get_quotes():
 #     endpoint = 'https://favqs.com/api/qotd'
 #     headers = {'Authorization': 'application/json'}
 #     response = requests.get(endpoint, headers).json()
@@ -24,32 +23,46 @@ def main():
 
 def get_quotes():
     keyword = keyword_search()
+    if keyword == 'done':
+        exit()
+    page = 1
     endpoint = f'https://favqs.com/api/quotes?page=<page>&filter=<{keyword}>'
     load_dotenv()
-    HEY = os.getenv('HELLO')
-    headers1 = {'token': HEY}
-    headers = {f'Authorization: Token token="{headers1["token"]}"': 'application/json'}
-    response = requests.get(endpoint, headers)
-    print(response)
+    key = os.getenv('QUOTE_KEY')
+    headers = {'Authorization': f'Token token="{key}"'}
+    params = {'page' : page}
+    response = requests.get(endpoint, headers=headers, params=params)
+    print(response.json()['quotes'][0]['body'])
+    next_page = True
+    while next_page == True: 
+        page_forward = input("Enter 'next page' or 'done' to exit:  ")
+        if page_forward == 'next page':
+            next_page = True
+            page = page + 1
+            params = {'page' : page}
+        else:
+            next_page = False
+        response = requests.get(endpoint, headers=headers, params=params)
+        print(response.json()['quotes'][0]['body'])
     return 0
 
 def keyword_search():
     keyword = input("Enter a keyword to search for related quotes or 'done' to exit:  ")
     return keyword
 
-def user_display():
-    get_quotes()
-    valid = 0
-    go_to_next = 0
-    while not valid:
-        next_page = input("Enter 'next page' or 'done':  ")
-        if next_page == 'done':
-            valid = 1
-        elif next_page == 'next page':
-            go_to_next = 1
-            #go to next page
-        else: print("Please enter a valid response")
-    return 0
+# def user_display():
+#     get_quotes()
+#     valid = 0
+#     go_to_next = 0
+#     while not valid:
+#         next_page = input("Enter 'next page' or 'done':  ")
+#         if next_page == 'done':
+#             valid = 1
+#         elif next_page == 'next page':
+#             go_to_next = 1
+#             #go to next page
+#         else: print("Please enter a valid response")
+#     return 0
 
 
 main()
