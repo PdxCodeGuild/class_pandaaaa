ea0ffff5a310ed79588cf17b2ab982002ff34626
Ashton Smith <ashton23@pdx.edu>
2021-07-19 16:28:18 -0700

    initial commit

---
+++ Code/AshtonSmith/django_labs/test folder/myproject/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+# Create your views here.
+def home(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies':movies
+    }
+    # return HttpResponse('<h1>hello world</h1>')
+    return render(request, 'myapp/index.html', context)
