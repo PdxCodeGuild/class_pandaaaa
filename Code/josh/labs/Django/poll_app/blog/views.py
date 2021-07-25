46f9a5c4abdaf62a72866e21ab6a8a47648112e7
jhell85 <jhell85@gmail.com>
2021-07-25 10:18:24 -0700

    add create post urls views and html

--- Code/josh/labs/Django/poll_app/blog/views.py
+++ Code/josh/labs/Django/poll_app/blog/views.py
@@ -1,7 +1,18 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.utils import timezone
+
+from .forms import PostForm
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def create_view(request):
+    form = PostForm(request.POST or None)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/')
+    return render(request, "blog/create_post.html", context)
