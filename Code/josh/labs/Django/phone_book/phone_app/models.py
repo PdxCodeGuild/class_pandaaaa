4356d2510a26d54d2d61ce65209fc05588eae13d
jhell85 <jhell85@gmail.com>
2021-07-28 16:28:13 -0700

    Mob code for phone book

---
+++ Code/josh/labs/Django/phone_book/phone_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=200, default="J Doe")
+    phone_number= PhoneNumberField()
+    address= models.CharField(max_length=200, default="address")
+    email= models.EmailField(max_length=254)
+    def __str__(self):
+      return self.name
