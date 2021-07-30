384f103f18dbd37abf65ad2392209f73f3ce3017
SB <sarahbtracy17@gmail.com>
2021-07-30 18:13:11 -0400

    adding to do

---
+++ 3 Django/solutions/todo_solution/todo_app/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Priority(models.Model):
+    PRIORITY = (
+            ('high', 'HIGH'),
+            ('low', 'LOW'), 
+            ('medium', 'MEDIUM'),
+            )
+    name = models.CharField(max_length = 15, choices = PRIORITY, blank = True, default = 'low')
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=12)
+    status = models.BooleanField(default = False)
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length = 200)
+    text = models.TextField(max_length = 500)
+    status = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    completed_at = models.DateTimeField(blank = True, null = True)
+    task_duration = models.DecimalField(max_digits = 4, decimal_places = 2, blank = True, null = True)
+
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE, null=True, blank=True)
+
+    TODO_TYPE = (
+        ('p', 'personal'),
+        ('s', 'school'),
+        ('w', 'work'),
+        ('f', 'family'),
+        ('m', 'misc'),
+    )
+    todo_type = models.CharField(max_length = 1, choices = TODO_TYPE, blank = True, default = 'm')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title', 'created_at']
