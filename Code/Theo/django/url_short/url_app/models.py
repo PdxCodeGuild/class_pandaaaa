4c0999d67c7ec3aa2215d97f81b3a6a7b48b804a
theorowlett <trowlett@gmail.com>
2021-08-02 15:50:11 -0700

    Added links and counter for links

--- Code/Theo/django/url_short/url_app/models.py
+++ Code/Theo/django/url_short/url_app/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Shortened_Url(models.Model):
     code = models.CharField(max_length=12)
     url = models.URLField(max_length=256)
+    counter = models.IntegerField(default=0)
     def __str__(self) -> str:
         return self.url
