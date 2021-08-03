62aab0a70eded26a4e7ea1fdaea0557c92389109
Ashton Smith <ashton23@pdx.edu>
2021-08-03 09:33:41 -0700

    .

--- Code/AshtonSmith/django_labs/lab2/lab2_proj/todo_app/urls.py
+++ Code/AshtonSmith/django_labs/lab2/lab2_proj/todo_app/urls.py
@@ -7,11 +7,25 @@ urlpatterns = [
     path('', views.home, name= 'home'),
     path('detailed/<int:pk>', views.TodoDetailed.as_view(), name= 'completed'),
     path('completed/detailed/<int:pk>', views.TodoDetailed.as_view(), name= 'completed'),
+
+    path('medium/detailed/<int:pk>', views.TodoDetailed.as_view(), name= 'completed'),
+    path('low/detailed/<int:pk>', views.TodoDetailed.as_view(), name= 'completed'),
+    path('high/detailed/<int:pk>', views.TodoDetailed.as_view(), name= 'completed'),
+
     path('completed/', views.completed, name= 'completed'),
     path('add/', views.add, name= 'add'),
     path('edit/<int:pk>/', views.TodoUpdate.as_view(), name= 'edit'),
+
     path('completed/edit/<int:pk>/', views.TodoUpdate.as_view(), name= 'edit'),
+    path('high/edit/<int:pk>/', views.TodoUpdate.as_view(), name= 'edit'),
+    path('medium/edit/<int:pk>/', views.TodoUpdate.as_view(), name= 'edit'),
+    path('low/edit/<int:pk>/', views.TodoUpdate.as_view(), name= 'edit'),
+
     path('delete/<int:pk>/' ,views.delete, name= 'delete'),
+    
+    path('high/delete/<int:pk>/' ,views.delete, name= 'delete'),
+    path('medium/delete/<int:pk>/' ,views.delete, name= 'delete'),
+    path('low/delete/<int:pk>/' ,views.delete, name= 'delete'),
     
     path('high/', views.high, name= 'high'),
     path('medium/', views.medium, name= 'medium'),
