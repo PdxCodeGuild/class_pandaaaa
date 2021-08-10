dd1703e0252480e4e84671c31641e8a42901f0a1
Zach <zach.watts13@gmail.com>
2021-08-10 12:59:22 -0400

    updating labs

--- Code/Zach/PDX_6_14/myproject/myproject/myapp/forms 2.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import MyModel
-class NumForm(forms.ModelForm):
-    class Meta:
-        model = MyModel
-        fields = ("numbers",)
--- Code/Zach/PDX_6_14/myproject/myproject/myapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class MyModel(models.Model): 
-    numbers = models.IntegerField(default=0)   
-    def __str__(self):
-        return self.numbers
