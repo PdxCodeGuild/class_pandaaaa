7d1e9674bcbff470e102e759b8a69c920d62e933
jhell85 <jhell85@gmail.com>
2021-08-19 21:34:27 -0700

    finished making UIcards dict

--- Code/josh/labs/JavaScript/BlackJack_Dom/scripts.js
+++ Code/josh/labs/JavaScript/BlackJack_Dom/scripts.js
@@ -61,53 +61,53 @@ UIDeck = {
   "King ❤️": `<span class='card rank-k hearts'><span class='rank'>K</span><span class='suit'>&hearts;</span></span>`,
   "King ♦️": `<span class='card rank-k diams'><span class='rank'>K</span><span class='suit'>&diams;</span></span>`,
   "Queen ♣️": `<span class='card rank-q clubs'><span class='rank'>Q</span><span class='suit'>&clubs;</span></span>`,
-  "Queen ♠️": 10,
-  "Queen ❤️": 10,
-  "Queen ♦️": 10,
+  "Queen ♠️": `<span class='card rank-q spades'><span class='rank'>Q</span><span class='suit'>&spades;</span></span>`,
+  "Queen ❤️": `<span class='card rank-q hearts'><span class='rank'>Q</span><span class='suit'>&hearts;</span></span>`,
+  "Queen ♦️": `<span class='card rank-q diams'><span class='rank'>Q</span><span class='suit'>&diams;</span></span>`,
   "Jack ♣️": `<span class='card rank-j clubs'><span class='rank'>J</span><span class='suit'>&clubs;</span></span>`,
-  "Jack ♠️": 10,
-  "Jack ❤️": 10,
-  "Jack ♦️": 10,
+  "Jack ♠️": `<span class='card rank-j spades'><span class='rank'>J</span><span class='suit'>&spades;</span></span>`,
+  "Jack ❤️": `<span class='card rank-j hearts'><span class='rank'>J</span><span class='suit'>&hearts;</span></span>`,
+  "Jack ♦️": `<span class='card rank-j diams'><span class='rank'>J</span><span class='suit'>&diams;</span></span>`,
   "10 ♣️": `<span class='card rank-10 clubs'><span class='rank'>10</span><span class='suit'>&clubs;</span></span>`,
-  "10 ♠️": 10,
-  "10 ❤️": 10,
-  "10 ♦️": 10,
+  "10 ♠️": `<span class='card rank-10 spades'><span class='rank'>10</span><span class='suit'>&spades;</span></span>`,
+  "10 ❤️": `<span class='card rank-10 hearts'><span class='rank'>10</span><span class='suit'>&hearts;</span></span>`,
+  "10 ♦️": `<span class='card rank-10 diams'><span class='rank'>10</span><span class='suit'>&diams;</span></span>`,
   "9 ♣️": `<span class='card rank-9 clubs'><span class='rank'>9</span><span class='suit'>&clubs;</span></span>`,
-  "9 ♠️": 9,
-  "9 ❤️": 9,
-  "9 ♦️": 9,
+  "9 ♠️": `<span class='card rank-9 spades'><span class='rank'>9</span><span class='suit'>&spades;</span></span>`,
+  "9 ❤️": `<span class='card rank-9 hearts'><span class='rank'>9</span><span class='suit'>&hearts;</span></span>`,
+  "9 ♦️": `<span class='card rank-9 diams'><span class='rank'>9</span><span class='suit'>&diams;</span></span>`,
   "8 ♣️": `<span class='card rank-8 clubs'><span class='rank'>8</span><span class='suit'>&clubs;</span></span>`,
-  "8 ♠️": 8,
-  "8 ❤️": 8,
-  "8 ♦️": 8,
+  "8 ♠️": `<span class='card rank-8 spades'><span class='rank'>8</span><span class='suit'>&spades;</span></span>`,
+  "8 ❤️": `<span class='card rank-8 hearts'><span class='rank'>8</span><span class='suit'>&hearts;</span></span>`,
+  "8 ♦️": `<span class='card rank-8 diams'><span class='rank'>8</span><span class='suit'>&diams;</span></span>`,
   "7 ♣️": `<span class='card rank-7 clubs'><span class='rank'>7</span><span class='suit'>&clubs;</span></span>`,
-  "7 ♠️": 7,
-  "7 ❤️": 7,
-  "7 ♦️": 7,
+  "7 ♠️": `<span class='card rank-7 spades'><span class='rank'>7</span><span class='suit'>&spades;</span></span>`,
+  "7 ❤️": `<span class='card rank-7 hearts'><span class='rank'>7</span><span class='suit'>&hearts;</span></span>`,
+  "7 ♦️": `<span class='card rank-7 diams'><span class='rank'>7</span><span class='suit'>&diams;</span></span>`,
   "6 ♣️": `<span class='card rank-6 clubs'><span class='rank'>6</span><span class='suit'>&clubs;</span></span>`,
-  "6 ♠️": 6,
-  "6 ❤️": 6,
-  "6 ♦️": 6,
+  "6 ♠️": `<span class='card rank-6 spades'><span class='rank'>6</span><span class='suit'>&spades;</span></span>`,
+  "6 ❤️": `<span class='card rank-6 hearts'><span class='rank'>6</span><span class='suit'>&hearts;</span></span>`,
+  "6 ♦️": `<span class='card rank-6 diams'><span class='rank'>6</span><span class='suit'>&diams;</span></span>`,
   "5 ♣️": `<span class='card rank-5 clubs'><span class='rank'>5</span><span class='suit'>&clubs;</span></span>`,
-  "5 ♠️": 5,
-  "5 ❤️": 5,
-  "5 ♦️": 5,
+  "5 ♠️": `<span class='card rank-5 spades'><span class='rank'>5</span><span class='suit'>&spades;</span></span>`,
+  "5 ❤️": `<span class='card rank-5 hearts'><span class='rank'>5</span><span class='suit'>&hearts;</span></span>`,
+  "5 ♦️": `<span class='card rank-5 diams'><span class='rank'>5</span><span class='suit'>&diams;</span></span>`,
   "4 ♣️": `<span class='card rank-4 clubs'><span class='rank'>4</span><span class='suit'>&clubs;</span></span>`,
-  "4 ♠️": 4,
-  "4 ❤️": 4,
-  "4 ♦️": 4,
+  "4 ♠️": `<span class='card rank-4 spades'><span class='rank'>4</span><span class='suit'>&spades;</span></span>`,
+  "4 ❤️": `<span class='card rank-4 hearts'><span class='rank'>4</span><span class='suit'>&hearts;</span></span>`,
+  "4 ♦️": `<span class='card rank-4 diams'><span class='rank'>4</span><span class='suit'>&diams;</span></span>`,
   "3 ♣️": `<span class='card rank-3 clubs'><span class='rank'>3</span><span class='suit'>&clubs;</span></span>`,
-  "3 ♠️": 3,
-  "3 ❤️": 3,
-  "3 ♦️": 3,
+  "3 ♠️": `<span class='card rank-3 spades'><span class='rank'>3</span><span class='suit'>&spades;</span></span>`,
+  "3 ❤️": `<span class='card rank-3 hearts'><span class='rank'>3</span><span class='suit'>&hearts;</span></span>`,
+  "3 ♦️": `<span class='card rank-3 diams'><span class='rank'>3</span><span class='suit'>&diams;</span></span>`,
   "2 ♣️": `<span class='card rank-2 clubs'><span class='rank'>2</span><span class='suit'>&clubs;</span></span>`,
-  "2 ♠️": 2,
-  "2 ❤️": 2,
-  "2 ♦️": 2,
+  "2 ♠️": `<span class='card rank-2 spades'><span class='rank'>2</span><span class='suit'>&spades;</span></span>`,
+  "2 ❤️": `<span class='card rank-2 hearts'><span class='rank'>2</span><span class='suit'>&hearts;</span></span>`,
+  "2 ♦️": `<span class='card rank-2 diams'><span class='rank'>2</span><span class='suit'>&diams;</span></span>`,
   "Ace ♣️": `<span class='card rank-a clubs'><span class='rank'>A</span><span class='suit'>&clubs;</span></span>`,
-  "Ace ♠️": 1,
-  "Ace ❤️": 1,
-  "Ace ♦️": 1,
+  "Ace ♠️": `<span class='card rank-a spades'><span class='rank'>A</span><span class='suit'>&spades;</span></span>`,
+  "Ace ❤️": `<span class='card rank-a hearts'><span class='rank'>A</span><span class='suit'>&hearts;</span></span>`,
+  "Ace ♦️": `<span class='card rank-a diams'><span class='rank'>A</span><span class='suit'>&diams;</span></span>`,
 };
 
 const populateDeck = () => {
@@ -166,8 +166,17 @@ const evaluateAces = (total, aces) => {
   //  you would only want 1 Ace counted as 11 because any more and you would bust
   return total + 10;
 };
-
 // Dom Functions
+let hitBtn = document.querySelector(".hit");
+let playerHand = document.getElementById("player");
+
+let hitBtnClicked = function () {
+  let cardUI = document.createElement("span");
+  cardUI.innerHTML = UIDeck["Ace ♣️"];
+  playerHand.appendChild(cardUI);
+};
+
+hitBtn.addEventListener("click", hitBtnClicked);
 
 // main Function
 const playBlackJack = () => {
@@ -182,9 +191,9 @@ const playBlackJack = () => {
   let dealerAces = countAces(dealerHand);
   let dealerHandWithAces = evaluateAces(dealerTotal, dealerAces);
 
-  console.log("handWithAces", handWithAces);
-  console.log("dealerHandWithAces", dealerHandWithAces);
-  console.log("deck", deck);
+  // console.log("handWithAces", handWithAces);
+  // console.log("dealerHandWithAces", dealerHandWithAces);
+  // console.log("deck", deck);
 };
 
 playBlackJack();
