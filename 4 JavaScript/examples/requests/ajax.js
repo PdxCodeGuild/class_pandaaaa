5cbf966a192a070c8567c26e5bdf83a86b60f445
sbtries <sarahbtracy17@gmail.com>
2021-08-23 14:05:04 -0400

    adding requests ex

---
+++ 4 JavaScript/examples/requests/ajax.js
@@ -0,0 +1,34 @@
+let target = document.getElementById("target");
+let textTarget = document.getElementById("textTarget")
+let resultHTML = ''
+
+
+
+let btn = document.getElementById('showQuote')
+let setText = function(){
+    let req = new XMLHttpRequest();
+    req.addEventListener("progress", function(e) {
+        console.log(e.loaded);
+        target.innerText = "Loading...";
+    });
+    req.addEventListener("error", function(e) {
+        console.log(e.status);
+        target.innerText = "Cannot load quote. Try again later!";
+    });
+    req.addEventListener("load", function(e) {
+        console.log(req.responseText);
+        let response = JSON.parse(req.responseText);
+        console.log(response);
+        let resultHTML = `
+            <p>${response.quote.body}</p>
+            <p><i><a href="${response.quote.url}">${response.quote.author}</a></i></p>
+            `
+        textTarget.innerText = response.quote.body
+    });
+    req.open("GET", "https://favqs.com/api/qotd");
+    req.setRequestHeader("Authorization", `Token token="${token}"`);
+    req.send()
+}
+
+btn.addEventListener('click', setText)
+
---
+++ 4 JavaScript/examples/requests/axios.js
@@ -0,0 +1,26 @@
+let target = document.getElementById("target");
+let textTarget = document.getElementById("textTarget")
+
+let getData = function(){ 
+axios.get('https://favqs.com/api/qotd', {
+    headers: { 
+    Authorization: `Token token="${token}"`
+    }
+}).then(response => {
+console.log(response.data)
+    let data = response.data
+    let quote = data.quote
+    let quoteDiv = document.createElement('div')
+    quoteDiv.innerText = quote.body
+    target.appendChild(quoteDiv)
+})
+}
+
+let btn = document.getElementById('showQuote')
+// let setText = function(){
+//     textTarget.innerHTML = resultHTML;
+//     console.log(resultHTML)
+//     console.log(textTarget)
+// }
+btn.addEventListener('click', getData)
+
