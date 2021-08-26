52d275d11ec12d27781a7f3632367c7ba6ee6cc8
sbtries <sarahbtracy17@gmail.com>
2021-08-26 16:45:03 -0400

    adding todo solution

---
+++ 4 JavaScript/examples/vue.js/todo/scripts.js
@@ -0,0 +1,24 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        todo: '',
+        todos: [],
+    },
+    methods: {
+        addNewTodo: function() {
+            console.log('hello')
+            let todo = {
+                title: this.todo,
+                completed: false,
+                editable: false
+            }
+            this.todos.push(todo);
+        },
+        removeTodo: function(index) {
+            this.todos.splice(index, 1);
+        },
+        youSure: function(){
+            this.isHidden = !this.isHidden;
+        }
+    }
+});
