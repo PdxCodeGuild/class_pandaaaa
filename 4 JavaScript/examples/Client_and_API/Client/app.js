21f9c8450e1e771c7acc20983f41d8e3056821d0
sbtries <sarahbtracy17@gmail.com>
2021-08-31 10:34:50 -0400

    adding client & api ex

---
+++ 4 JavaScript/examples/Client_and_API/Client/app.js
@@ -0,0 +1,17 @@
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+
+var app = new Vue({
+    el: "#app",
+
+    data: {
+      allBoletes: [],
+      currentRoute: window.location.pathname
+    },
+    mounted: function () {
+      // get all dog breeds from api to populate form select
+      axios.get("http://127.0.0.1:8000/api").then(function (response) {
+        app.allBoletes = response.data;
+      });
+    },
+  });
